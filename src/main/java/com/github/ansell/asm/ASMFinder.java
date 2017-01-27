7e0bb71e4ba00398bd6c527fbd5564131f9b275a
Peter Ansell <p_ansell@yahoo.com>
2017-01-27 16:07:12 +1100

    Initial stub for ASMFinder
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

---
+++ src/main/java/com/github/ansell/asm/ASMFinder.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2017, Peter Ansell
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.github.ansell.asm;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import joptsimple.OptionException;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+
+/**
+ * Finds references in JAR files using ASM.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+public final class ASMFinder {
+
+	/**
+	 * Private constructor for static only class
+	 */
+	private ASMFinder() {
+	}
+
+	public static void main(String... args) throws Exception {
+		final OptionParser parser = new OptionParser();
+
+		final OptionSpec<Void> help = parser.accepts("help").forHelp();
+		final OptionSpec<File> input = parser.accepts("input").withRequiredArg().ofType(File.class).required()
+				.describedAs("The input JAR file to be searched.");
+		final OptionSpec<Boolean> debug = parser.accepts("debug").withRequiredArg().ofType(Boolean.class)
+				.defaultsTo(Boolean.FALSE).describedAs("Set to true to debug.");
+
+		OptionSet options = null;
+
+		try {
+			options = parser.parse(args);
+		} catch (final OptionException e) {
+			System.out.println(e.getMessage());
+			parser.printHelpOn(System.out);
+			throw e;
+		}
+
+		if (options.has(help)) {
+			parser.printHelpOn(System.out);
+			return;
+		}
+
+		final Path inputPath = input.value(options).toPath();
+		if (!Files.exists(inputPath)) {
+			throw new FileNotFoundException("Could not find input JAR file: " + inputPath.toString());
+		}
+
+	}
+
+}
